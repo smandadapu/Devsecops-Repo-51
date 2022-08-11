108fa293cb017580a9b800ae6ea977c3da3f9800
satya <smandadapu.mail@gmail.com>
2022-08-11 20:09:27 +0530

    change from DEV-02

--- HSBC-LIFE-DIGITALAPPS/src/main/java/jar/App.java
+++ HSBC-LIFE-DIGITALAPPS/src/main/java/jar/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "change from DEV02" );
 		System.out.println( "user-story-1" );
     }
 }
