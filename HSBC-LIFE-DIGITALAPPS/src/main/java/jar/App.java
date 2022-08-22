d1bcded68447a70bd32b17b57bcc9299a3cca187
satya <smandadapu.mail@gmail.com>
2022-08-22 09:57:47 +0530

    Develop 1.0 (#2)
    * update from DEV-01
    
    * change from DEV-02

--- HSBC-LIFE-DIGITALAPPS/src/main/java/jar/App.java
+++ HSBC-LIFE-DIGITALAPPS/src/main/java/jar/App.java
@@ -8,7 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "change from DEV02" );
+        System.out.println( "change from DEV01" );
 		System.out.println( "user-story-1" );
     }
 }
